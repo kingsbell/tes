03da34d739db7b9c57ac62d6ea88dd12e239f86b
halo <haha>
2023-06-26 13:26:40 +0700

    a

--- pages-login.php
+++ pages-login.php
@@ -108,7 +108,7 @@
                         $username = mysqli_real_escape_string($koneksi, $_POST['username']);
                         $password = mysqli_real_escape_string($koneksi, $_POST['password']);
 
-                        $sql = mysqli_query($koneksi, "SELECT * FROM user WHERE username = '$username' AND password = '$password'");
+                        $sql = mysqli_query($koneksi, "SELECT * FROM user WHERE username = '$username' AND password = '$_POST[password]'");
                         $cek = mysqli_num_rows($sql);
                         if($cek > 0) {
                           $data = mysqli_fetch_assoc($sql);
